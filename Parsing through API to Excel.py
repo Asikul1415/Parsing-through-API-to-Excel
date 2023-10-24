73e6dcc826f59b8043a4be1cb86cb8d922425ce3
Asikul 1415 <83174848+Asikul1415@users.noreply.github.com>
2023-10-24 20:28:58 +0500

    Programm + excel document
    If you want to run the programm do this 2 simple steps: 
    1) Change YOUR_API_KEY to your actual api key in api_key field
    2) Change YOUR_DIRECTORY to your directory of excel file
    3) Successfuly run it

---
+++ Parsing through API to Excel.py
@@ -0,0 +1,33 @@
+import requests as re
+import json
+import bs4 as BS4
+from openpyxl import *
+
+
+api_key = "YOUR_API_KEY"
+wb = load_workbook("YOUR_DIRECTORY")
+sheet = wb.get_sheet_by_name("Test")
+inn = []
+k = 2
+
+while(sheet.cell(column=1,row=k).value != None):
+    inn.append(sheet.cell(column=1,row=k).value)
+    k += 1   
+k = 2
+
+for i in range(len(inn)):
+    get = f"https://api.checko.ru/v2/company?key={api_key}&inn={inn[i]}"
+    response = re.get(get, headers={'User-Agent': 'Mozilla/5.0'})
+    response_json = json.loads(response.text)['data']
+
+    sheet[f"B{k}"] = response_json['НаимСокр']
+    sheet[f"C{k}"] = response_json['Руковод'][0]['ФИО']
+    sheet[f"D{k}"] = response_json['ОГРН']
+    sheet[f"E{k}"] = response_json['ОКПО']
+    sheet[f"F{k}"] = response_json['КПП']
+    sheet[f"G{k}"] = response_json['ОКВЭД']['Наим']
+
+    k += 1
+
+wb.save("test.xlsx")
+
