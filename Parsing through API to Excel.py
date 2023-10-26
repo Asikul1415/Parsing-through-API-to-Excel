6d30b33df3be7540e02f2d0288621256612955bd
Asikul1415 <asikulsteam@yandex.ru>
2023-10-26 23:55:41 +0500

    Added error handling if ИНН does not exist

--- Parsing through API to Excel.py
+++ Parsing through API to Excel.py
@@ -3,7 +3,6 @@ import json
 import bs4 as BS4
 from openpyxl import *
 
-
 api_key = "YOUR_API_KEY"
 wb = load_workbook("YOUR_DIRECTORY")
 sheet = wb.get_sheet_by_name("Test")
@@ -20,13 +19,17 @@ for i in range(len(inn)):
     response = re.get(get, headers={'User-Agent': 'Mozilla/5.0'})
     response_json = json.loads(response.text)['data']
 
-    sheet[f"B{k}"] = response_json['НаимСокр']
-    sheet[f"C{k}"] = response_json['Руковод'][0]['ФИО']
-    sheet[f"D{k}"] = response_json['ОГРН']
-    sheet[f"E{k}"] = response_json['ОКПО']
-    sheet[f"F{k}"] = response_json['КПП']
-    sheet[f"G{k}"] = response_json['ОКВЭД']['Наим']
-
+    try:
+        sheet[f"B{k}"] = response_json['НаимСокр']
+        sheet[f"C{k}"] = response_json['Руковод'][0]['ФИО']
+        sheet[f"D{k}"] = response_json['ОГРН']
+        sheet[f"E{k}"] = response_json['ОКПО']
+        sheet[f"F{k}"] = response_json['КПП']
+        sheet[f"G{k}"] = response_json['ОКВЭД']['Наим']
+    except:
+        k += 1
+        continue
+    
     k += 1
 
 wb.save("test.xlsx")
